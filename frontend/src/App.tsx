f18e2016d290d36f3946b4391a2eb15b70668d59
Chet Jogia <chetjogia@gmail.com>
2023-01-24 12:43:49 +0000

    added fetch to obtain distinct care recipients

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,26 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Search from "./Components/Search";
 import Summary from "./Components/Summary";
 
 function App() {
+  const [distinctUsers, setDistinctUsers] = useState()
+
+
+  useEffect( ()=>{
+    getDistinctUsers()
+  }, [])
+
+  async function getDistinctUsers(){
+    const response = await fetch('http://localhost:8000/distinctusers')
+    const data = await response.json()
+    setDistinctUsers(data)
+  }
+
+
+  
+  console.log("Distinct USERS", distinctUsers)
+
   return (
     <div className="App">
       <div className="top-dashboard">
