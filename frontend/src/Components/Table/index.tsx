b05da02253a053a3e7903d3949a0cffc790bca44
Chet Jogia <chetjogia@gmail.com>
2023-01-25 01:37:31 +0000

    layout of instructions complete

--- frontend/src/Components/Table/index.tsx
+++ frontend/src/Components/Table/index.tsx
@@ -73,17 +73,17 @@ function Table(props: tableProps) {
     //render instructions if payload is not present
     return (
       <div className="main-table">
-        <div className="table-container">
+        <div className="instructions">
           <h1>Instructions:</h1>
           <p><strong>Step 1: </strong>Choose a care recipient</p>
          <p> <strong>Step 2:</strong> Complete remaining search criteria as desired to see data for
           care recipient (Note: Date range is required)</p> 
-          <p>Example criteria: </p>
-            <p><strong>Care Recipient: </strong>"df50cac5-293c-490d-a06c-ee26796f850d"</p>, 
-            <p><strong>Event Type:</strong> "food_intake_observation"</p>, 
-            <p><strong>Care Giver:</strong> "all"</p>, 
+          <h2>Example criteria: </h2>
+            <p><strong>Care Recipient: </strong>"df50cac5-293c-490d-a06c-ee26796f850d"</p>
+            <p><strong>Event Type:</strong> "food_intake_observation"</p>
+            <p><strong>Care Giver:</strong> "all"</p>
             <p><strong>Date From:</strong> "01/01/2019"</p> 
-            <p><strong>Date Until:</strong> "24/01/2022"</p>
+            <p><strong>Date Until:</strong> "24/01/2023"</p>
         </div>
       </div>
     );
